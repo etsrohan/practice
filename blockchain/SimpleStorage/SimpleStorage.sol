8eebc912cbda1656c4cefc1b49c2ba4e261162f2
nexgrohan <rohan.srivastava@nexg.in>
2022-02-28 17:28:13 +0530

    First break

---
+++ blockchain/SimpleStorage/SimpleStorage.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+
+    uint256 favoriteNumber = 5;
+    bool favoriteBool = false;
+    string favoriteString = "String";
+    int256 favoriteInt = -5;
+    address favoriteAddress = 0xbB66eF34814f0613a3B738288FE55553A69C44BA;
+    bytes32 favoriteBytes = "Cat";
+
+}
---
+++ blockchain/SimpleStorage/test.py
@@ -0,0 +1,6 @@
+import solcx
+
+print(f"""
+{solcx.get_installable_solc_versions()}
+{solcx.get_installed_solc_versions()}
+""")
