8845bc8ff3b4b5b35529a715cb4f66cebffa6531
nexgrohan <rohan.srivastava@nexg.in>
2022-03-21 01:16:00 +0530

    Successfully deployed mocks and everything is working. Note: re-deploy onto local ganache chain

--- blockchain/BrownieFundMe/scripts/deploy.py
+++ blockchain/BrownieFundMe/scripts/deploy.py
@@ -1,22 +1,31 @@
-from brownie import accounts, FundMe, network, config
-from scripts.helpful_scripts import get_account, get_aggregator
+from brownie import accounts, FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import (
+    deploy_mocks,
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+from web3 import Web3
 
 
 def deploy_fund_me():
     admin = get_account()
-    # Pass the price feed address to our fundme contract
 
-    if network.show_active() != "development":
+    # Pass the price feed address to our fundme contract
+    # if we are on a persistent network like rinkeby, use the associated address
+    # otherwise, deploy mocks
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]
     else:
-        pass
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
 
     fund_me = FundMe.deploy(
         price_feed_address,
         {"from": admin},
-        publish_source=True,
+        publish_source=config["networks"][network.show_active()].get("verify"),
     )
     print(f"Contract deployed to {fund_me.address}")
 
